8cc0c77dd35ba4f169058e7c8425dd619280374e
Johnny Pena <jpenaguitarist@gmail.com>
2025-06-01 18:59:53 -0500

    Added min-h-screen CSS to keep the footer at the bottom of contact.jsx

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -16,7 +16,7 @@ export const meta = () => {
 
 export default function Contact() {
     return (
-        <div className="bg-base-200 flex flex-col justify-center items-center pt-20 px-4 w-full">
+        <div className="bg-base-200 flex flex-col justify-center items-center pt-20 px-4 w-full min-h-screen">
             <h1 className="text-center text-5xl font-bold mb-4">Contact Us</h1>
                 <ContactForm />
         </div>
