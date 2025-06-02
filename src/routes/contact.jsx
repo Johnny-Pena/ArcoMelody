ff87fdb7695b1505629ebfaa0fa92cc5fcc5378c
Johnny Pena <jpenaguitarist@gmail.com>
2025-06-01 19:07:14 -0500

    added a medium screen breakpoint to the min-h-screen CSS in contact.jsx

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -16,7 +16,7 @@ export const meta = () => {
 
 export default function Contact() {
     return (
-        <div className="bg-base-200 flex flex-col justify-center items-center pt-20 px-4 w-full min-h-screen">
+        <div className="bg-base-200 flex flex-col justify-center items-center pt-20 px-4 w-full md:min-h-screen">
             <h1 className="text-center text-5xl font-bold mb-4">Contact Us</h1>
                 <ContactForm />
         </div>
